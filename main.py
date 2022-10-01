c2e989d5b00c3658de3335ec0f9d431130524051
Abderaouf Louggani <ja_louggani@esi.dz>
2022-10-01 12:42:02 +0100

    adding origins

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Link(BaseModel):
     url: str
