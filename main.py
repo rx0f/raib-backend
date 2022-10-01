da5748a5f92ab98d1554e57d623ca9958f9ba2a9
Abderaouf Louggani <ja_louggani@esi.dz>
2022-10-01 16:23:21 +0100

    allowing all origins

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["https://kerberos.netlify.app/"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
